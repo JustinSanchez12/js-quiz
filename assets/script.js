6048e476505d8450395a1b245179c659a1c79f8d
Justin <justinlsanchez12@gmail.com>
2023-04-14 15:33:26 -0700

    Changed the javascript so it will display first question after you press start and timer goes down

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,49 @@
 var startButton = document.querySelector(".start-button");
 var highScore = document.querySelector(".high-score");
 
-var choicesEl = document.querySelector('.choices');
 var resultsEl = document.querySelector(".results");
-var questionsEl = document.querySelector(".questions");
-var correct;
+var questionEl = document.querySelector('.question');
+var choicesContainer = document.querySelector(".choices-container");
+
 
 var timerElement = document.querySelector(".timer-count");   //Timer variables
 var timer;
 var timeCount;
 
-function startGame() {
-    timeCount = 10;
-    startTimer();
-    question1();
-};
+var question = [
+    {
+        question: "What is 2 + 2?",
+        choices: ["1", "2","3","4"],
+        answer: 3
+    }
+];
 
-function question1() {
-    
-};
+choicesContainer.style.display = "none";
+startButton.style.display = "block";
+
+startButton.addEventListener("click", function() {
+    startButton.style.display = "none";
+    choicesContainer.style.display = "block";
+
+    displayQuestion(question[0]);
+    timeCount =10;
+    startTimer();
+});
+
+function displayQuestion(questionObj) {
+    var choicesContainer = document.querySelector('.choices-container');
+
+    choicesContainer.innerHTML = "";
+
+    questionEl.textContent = questionObj.question;
+
+    for (let i =0; i < questionObj.choices.length; i++) {
+        var choices = questionObj.choices[i];
+        var choiceBtn = document.createElement("button");
+        choiceBtn.textContent = choices;
+        choicesContainer.appendChild(choiceBtn);
+    }
+}
 
 //Timer starts when user presses the start button to start the game
 function startTimer() {
@@ -31,4 +56,4 @@ function startTimer() {
     }, 1000);
 };
 
-startButton.addEventListener("click", startGame);
+
