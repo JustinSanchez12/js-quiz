bb48930ffc17c4a336c32e0d9525ab2608cfcb0c
Justin Sanchez <justinlsanchez12@gmail.com>
2023-08-16 14:49:36 -0700

    updated the script so that it moves onto the next question after user answers first question correctly

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
 //Variables used in Javascript
-
 var startButton = document.querySelector(".start-button");
 var resultsButton = document.querySelector(".results-button");
 var highScore = document.querySelector(".high-score");
@@ -9,14 +8,13 @@ var resultsContainer = document.querySelector(".results-container");
 var questionEl = document.querySelector('.question');
 var choicesContainer = document.querySelector(".choices-container");
 
-var question2 = document.querySelector('.questtion');
-
-
-var timerElement = document.querySelector(".timer-count");   //Timer variables
+ //Timer variables
+var timerElement = document.querySelector(".timer-count");  
 var timer;
 var timeCount;
+// To keep track of the current question index
+var currentQuestionIndex = 0; 
 
-//This will be where the questions are stored and will just be called by a function
 var question = [ 
     {
         question: "What is 2 + 2?",
@@ -30,80 +28,77 @@ var question = [
     }
 ];
 
-//This will first display the start button and hide the questions
 choicesContainer.style.display = "none";
 startButton.style.display = "block";
 
-//When the user clicks on the start button, the first question will appear
 startButton.addEventListener("click", function() {
     startButton.style.display = "none";
     choicesContainer.style.display = "block";
 
     displayQuestion(question[0]);
-    timeCount =10;
+    timeCount = 10;
     startTimer();
 });
 
-//This should have had displayed the correct answer for the first question
 resultsButton.addEventListener("click", function(){
     resultsButton.style.display = "none";
     choicesContainer.style.display = "block";
 
-    displayResults(question.answer[0]);
+    displayResults(question[currentQuestionIndex].answer);
 });
 
-//Once the first question was answered this would've brought the user to the next question. Here displayQuestions comes up again but it will call the second question from the
-//variable question
-question2.addEventListener("click", function(){
-    choicesContainer.style.display = "block";
-
-    displayQuestion(question[1]);
-    timeCount = 10;
-    startTimer();
-})
-
-
-//Here is the function for displaying the question
 function displayQuestion(questionObj) {
     var choicesContainer = document.querySelector('.choices-container');
-  
+
     choicesContainer.innerHTML = "";
     questionEl.textContent = questionObj.question;
-  
-    for (let i = 0; i < questionObj.choices.length; i++) {
-      let choice = questionObj.choices[i];
-      let choiceBtn = document.createElement("button");
-      choiceBtn.textContent = choice;
-      choicesContainer.appendChild(choiceBtn);
-      choiceBtn.addEventListener("click", function() {
-        if (i === questionObj.answer) {
-          alert("Correct!");
-        } else {
-          alert("Incorrect!");
-        }
-        displayResults(choice); // Display the user's selected choice
-      });
-    }
-  }
-  
 
-//This should have displayed the answer
+    for (let i = 0; i < questionObj.choices.length; i++) {
+        let choice = questionObj.choices[i];
+        let choiceBtn = document.createElement("button");
+        choiceBtn.textContent = choice;
+        choicesContainer.appendChild(choiceBtn);
+        choiceBtn.addEventListener("click", function() {
+            if (i === questionObj.answer) {
+                alert("Correct!");
+                // Move to the next question
+                currentQuestionIndex++;
+                if (currentQuestionIndex < question.length) {
+                    displayQuestion(question[currentQuestionIndex]);
+                    timeCount = 10;
+                } else {
+                    // All questions answered
+                    alert("Quiz completed!");
+                }
+            } else {
+                alert("Incorrect!");
+            }
+            displayResults(choice);
+        });
+    }
+}
+
 function displayResults(answer) {
     resultsContainer.style.display = "block";
     resultsEl.textContent = "Your Answer: " + answer;
 }
 
-
-//Timer starts when user presses the start button to start the game
 function startTimer() {
-    timer = setInterval(function() { //sets time
-        timeCount--; //Decreases time by 1
+    timer = setInterval(function() {
+        timeCount--;
         timerElement.textContent = timeCount;
-        if(timeCount <= 0) {
+        if (timeCount <= 0) {
             clearInterval(timer);
             alert("Time's Up");
+            // Move to the next question
+            currentQuestionIndex++;
+            if (currentQuestionIndex < question.length) {
+                displayQuestion(question[currentQuestionIndex]);
+                timeCount = 10;
+            } else {
+                // All questions answered
+                alert("Quiz completed!");
+            }
         }
     }, 1000);
-};
-
-
+}
